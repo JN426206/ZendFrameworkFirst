569ebf894cb4a7f823b82e1cadc9ba31486addb5
Jan Nowak <jann998@gmail.com>
2018-04-15 22:16:21 +0200

    Dodanie modulu auth

---
+++ module/Auth/config/module.config.php
@@ -0,0 +1,38 @@
+<?php
+namespace Auth;
+
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    
+    'controllers' => [
+        'factories' => [
+            Controller\AuthController::class => InvokableFactory::class,
+        ],
+    ],
+    
+    'router' => [
+        'routes' => [
+            'auth' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/auth[/:action]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AuthController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    
+    'view_manager' => [
+        'template_path_stack' => [
+            'auth' => __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/Auth/src/Controller/AuthController.php
@@ -0,0 +1,13 @@
+<?php 
+namespace Auth\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AuthController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
