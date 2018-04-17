82621361ecf365f055adf0528595dd2987c3a896
Jan Nowak <jann998@gmail.com>
2018-04-17 23:07:06 +0200

    Zrobione filtrowanie dostepu. Tzn ograniczenie dostepu do poszczegolnych stron jezeli nie zalogowano sie.

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -23,7 +23,25 @@ return [
             ],
         ],
     ],
-    
+    'access_filter' => [
+        'options' => [
+            // The access filter can work in 'restrictive' (recommended) or 'permissive'
+            // mode. In restrictive mode all controller actions must be explicitly listed
+            // under the 'access_filter' config key, and access is denied to any not listed
+            // action for users not logged in. In permissive mode, if an action is not listed
+            // under the 'access_filter' key, access to it is permitted to anyone (even for
+            // users not logged in. Restrictive mode is more secure and recommended.
+            'mode' => 'restrictive'
+        ],
+        'controllers' => [
+            Controller\AlbumController::class => [
+                // Allow anyone to visit "index" and "about" actions
+                ['actions' => ['index'], 'allow' => '*'],
+                // Allow authenticated users to visit "settings" action
+                ['actions' => ['edit','delete'], 'allow' => '@']
+            ],
+        ]
+    ],
     'view_manager' => [
         'template_path_stack' => [
             'album' => __DIR__ . '/../view',
--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -51,7 +51,26 @@ return [
                 ],
             ],
         ],
-    ],    
+    ],        
+    'access_filter' => [
+        'options' => [
+            // The access filter can work in 'restrictive' (recommended) or 'permissive'
+            // mode. In restrictive mode all controller actions must be explicitly listed
+            // under the 'access_filter' config key, and access is denied to any not listed
+            // action for users not logged in. In permissive mode, if an action is not listed
+            // under the 'access_filter' key, access to it is permitted to anyone (even for
+            // users not logged in. Restrictive mode is more secure and recommended.
+            'mode' => 'restrictive'
+        ],
+        'controllers' => [
+            Controller\UserController::class => [
+                // Allow anyone to visit "index" and "about" actions
+                ['actions' => ['index'], 'allow' => '*'],
+                // Allow authenticated users to visit "settings" action
+                ['actions' => ['logout'], 'allow' => '@']
+            ],
+        ]
+    ],
     'service_manager' => [
         'factories' => [            
             \Zend\Authentication\AuthenticationService::class => Service\Factory\AuthenticationServiceFactory::class,
